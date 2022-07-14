48fabbb4af068e09dd03f3ea5b9cb14858a66249
AshishAcharya083 <imashish083@gmail.com>
2022-07-14 21:55:32 +0545

    Update Model

--- models.py
+++ models.py
@@ -1,5 +1,23 @@
 from app import  db
 from datetime import datetime
+import enum
+
+
+class User(db.model):
+    __table_name__ = 'user'
+    id: db.Column(db.Integer, primary_key=True)
+    first_name: db.Column(db.String(15))
+    email: db.Column(db.String(20))
+    password_hash: db.Column(db.String())
+    address: db.Column(db.String(50))
+
+
+class Status(enum.Enum):
+    selected = 1
+    in_process = 2
+    done = 3
+    rejected = 4
+    pending = 5
 
 
 class Complaint(db.model):
@@ -11,18 +29,18 @@ class Complaint(db.model):
     municipality_name = db.Column(db.String(50))
     ward_no = db.Column(db.Integer)
     created = db.Column(db.DateTime, default=datetime.now())
+    status = db.Column(db.String(), Status=Status.selected)
     created_by = db.Column(db.String(), db.ForeignKey("user.id"))
 
     def __init__(self):
         super().__init__()
 
 
-class User(db.model):
-    __table_name__ = 'user'
-    id: db.Column(db.Integer, primary_key=True)
-    first_name: db.Column(db.String(15))
-    email: db.Column(db.String(20))
-    password_hash: db.Column(db.String())
-    address: db.Column(db.String(50))
+
+
+
+
+
+
 
 

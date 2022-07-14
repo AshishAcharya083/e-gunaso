8337e793a032e63c1f4f2d7d6211bd6364cc589d
AshishAcharya083 <imashish083@gmail.com>
2022-07-14 21:33:34 +0545

    Add user model

--- models.py
+++ models.py
@@ -3,6 +3,7 @@ from datetime import datetime
 
 
 class Complaint(db.model):
+    __table_name__ = 'complaint'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(80))
     creator_id = db.Column(db.String(50))
@@ -10,8 +11,18 @@ class Complaint(db.model):
     municipality_name = db.Column(db.String(50))
     ward_no = db.Column(db.Integer)
     created = db.Column(db.DateTime, default=datetime.now())
+    created_by = db.Column(db.String(), db.ForeignKey("user.id"))
 
     def __init__(self):
         super().__init__()
+
+
+class User(db.model):
+    __table_name__ = 'user'
+    id: db.Column(db.Integer, primary_key=True)
+    first_name: db.Column(db.String(15))
+    email: db.Column(db.String(20))
+    password_hash: db.Column(db.String())
+    address: db.Column(db.String(50))
 
 
